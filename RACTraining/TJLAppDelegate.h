d4205cede4b18ed1906454a406c90991818edafd
Terry Lewis <terrylewis@outlook.com>
2014-03-01 19:37:45 -0600

    Initial Commit

---
+++ RACTraining/TJLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TJLAppDelegate.h
+//  RACTraining
+//
+//  Created by Terry Lewis II on 3/1/14.
+//  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
