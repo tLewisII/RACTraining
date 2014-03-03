417d6be3ebcbd2fe9fdf180b684ef967dccceafe
Terry Lewis <terrylewis@outlook.com>
2014-03-02 18:06:00 -0600

    transformation example

--- RACTraining/TJLViewController.m
+++ RACTraining/TJLViewController.m
@@ -9,82 +9,77 @@
 #import "TJLViewController.h"
 
 @interface TJLViewController ()
-@property(weak, nonatomic) IBOutlet UITextField *textField;
-@property(strong, nonatomic) NSString *pikachuString;
+@property(weak, nonatomic) IBOutlet UILabel *stateLabel;
+@property(weak, nonatomic) IBOutlet UILabel *translationLabel;
+
 @end
 
 @implementation TJLViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    CGPoint originalCenter = self.view.center;
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] init];
+    RACSignal *panGestureSignal = panGestureRecognizer.rac_gestureSignal;
+    [self.view addGestureRecognizer:panGestureRecognizer];
+    RACSignal *panGestureMapped = [panGestureSignal map:^id(UIPanGestureRecognizer *recognizer) {
+        CGPoint translation = [recognizer translationInView:recognizer.view];
+        CGFloat yBoundary = MIN(recognizer.view.center.y + translation.y, originalCenter.y);
+        [recognizer setTranslation:CGPointZero inView:self.view];
+        return [NSValue valueWithCGPoint:CGPointMake(recognizer.view.center.x, yBoundary)];
+    }];
+    RAC(self, view.center) = panGestureMapped;
 
-    /// Most signals do their work synchronously, and will deliver their results on the thread they started on
-    RACSignal *mainThreadSignal = [RACSignal createSignal:^RACDisposable *(id <RACSubscriber> subscriber) {
-        NSString *pikachu = @"pikachu";
-        if(NSThread.isMainThread) {
-            NSLog(@"%@ signal block executed on main thread", pikachu);
-        }
-        else {
-            NSLog(@"%@ signal block not executed on main thread", pikachu);
-        }
-        [subscriber sendNext:pikachu];
-        return nil;
+    ///We want to keep track of the y value of the translation and display it in real time to the user,
+    /// so we transform the value into a String and return that.
+    RACSignal *panGestureString = [panGestureMapped map:^id(NSValue *value) {
+        return [NSString stringWithFormat:@"Y Translation = %f", value.CGPointValue.y];
     }];
 
-    /// The signal did its work on the main thread, and delivers it work on the main thread.
-    [mainThreadSignal subscribeNext:^(id x) {
-        if(NSThread.isMainThread) {
-            NSLog(@"%@ subscription block executed on main thread", x);
+    ///We also want real time information about the state of the gesture,
+    /// so we transform that into a String as well. You have to move very slowly if you want to see the label update when the Gesture begins.
+    RACSignal *panGestureState = [panGestureSignal map:^id(UIPanGestureRecognizer *recognizer) {
+        NSString *state;
+        switch(recognizer.state) {
+            case UIGestureRecognizerStateBegan:
+                state = @"Gesture Began";
+                break;
+            case UIGestureRecognizerStateChanged:
+                state = @"Gesture is Changed";
+                break;
+            case UIGestureRecognizerStateEnded:
+                state = @"Gesture is Ended";
+                break;
+            default:
+                break;
+            case UIGestureRecognizerStatePossible:
+                break;
+            case UIGestureRecognizerStateCancelled:
+                break;
+            case UIGestureRecognizerStateFailed:
+                break;
         }
-        else {
-            NSLog(@"%@ subscription block not executed on main thread", x);
-        }
+        return state;
     }];
 
-    /// This signal does its work on the main thread, but will deliver its results on a background thread
-    RACSignal *backgroundThreadSignal = [[RACSignal createSignal:^RACDisposable *(id <RACSubscriber> subscriber) {
-        NSString *squirtle = @"squirtle";
-        if(NSThread.isMainThread) {
-            NSLog(@"%@ signal block executed on main thread", squirtle);
-        }
-        else {
-            NSLog(@"%@ signal block not executed on main thread", squirtle);
-        }
-        [subscriber sendNext:squirtle];
-        return nil;
-    }] deliverOn:[RACScheduler schedulerWithPriority:RACSchedulerPriorityBackground]];
-
-    [backgroundThreadSignal subscribeNext:^(id x) {
-        if(NSThread.isMainThread) {
-            NSLog(@"%@ subscription block executed on main thread", x);
-        }
-        else {
-            NSLog(@"%@ subscription block not executed on main thread", x);
-        }
+    ///We want a different color to represent the state of the gesture, so we use the states as keys.
+    NSDictionary *colors = @{@(UIGestureRecognizerStateEnded)   : [UIColor blueColor],
+                             @(UIGestureRecognizerStateChanged) : [UIColor purpleColor]};
+    ///Here we filter the values, and only return a color from our dictionary when the state is one of the ones we care about.
+    RACSignal *colorSignal = [[panGestureSignal filter:^BOOL(UIPanGestureRecognizer *recognizer) {
+        return (recognizer.state == UIGestureRecognizerStateChanged || recognizer.state == UIGestureRecognizerStateEnded);
+    }] map:^id(UIPanGestureRecognizer *recognizer) {
+        return colors[@(recognizer.state)];
     }];
-
-    /// This signal will do its work on a background thread.
-    RACSignal *lazySignal = [RACSignal startLazilyWithScheduler:[RACScheduler schedulerWithPriority:RACSchedulerPriorityBackground] block:^(id <RACSubscriber> subscriber) {
-        NSString *charizard = @"charizard";
-        if(NSThread.isMainThread) {
-            NSLog(@"%@ subscription block executed on main thread", charizard);
-        }
-        else {
-            NSLog(@"%@ subscription block not executed on main thread", charizard);
-        }
-
-        [subscriber sendNext:charizard];
-    }];
-
-    /// All we have to do to receive results on the main thread is call `-deliverOn:` with the main thread scheduler.
-    [[lazySignal deliverOn:RACScheduler.mainThreadScheduler] subscribeNext:^(id x) {
-        if(NSThread.isMainThread) {
-            NSLog(@"%@ subscription block executed on main thread", x);
-        }
-        else {
-            NSLog(@"%@ subscription block not executed on main thread", x);
-        }
-    }];
+    ///We want the starting point displayed when the view loads.
+    NSString *initialPoint = [NSString stringWithFormat:@"Y Translation = %f", originalCenter.y];
+    ///+[RACSignal merge] takes an array of signals and returns a value each time one of the signals fires.
+    /// Here the initialPoint immediately returns, thus the label is set when the view loads. Then afterwards, the panGestureSignal will be sending its values when it is activated.
+    RAC(self.translationLabel, text) = [RACSignal merge:@[panGestureString, [RACSignal return:initialPoint]]];
+    ///The label will always reflect the current state of the recognizer.
+    RAC(self.stateLabel, text) = panGestureState;
+    ///The color will be updated each time the signal returns a value.
+    RAC(self.view, backgroundColor) = colorSignal;
 }
 
 @end
