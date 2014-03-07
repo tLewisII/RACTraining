59a15e38c464caba3a3b18ad1129b9a75d9c5abd
Terry Lewis <terrylewis@outlook.com>
2014-03-07 11:00:20 -0600

    version 0.9 lifting example

--- RACTraining/TJLViewController.m
+++ RACTraining/TJLViewController.m
@@ -6,111 +6,48 @@
 //  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
 //
 
-#define GreenStar [UIImage imageNamed:@"GreenStar"]
-#define RedStar [UIImage imageNamed:@"RedStar"]
-
-#define pictureURL [NSURL URLWithString:@"https://farm9.staticflickr.com/8109/8631724728_48c13f7733_b.jpg"]
-
 #import "TJLViewController.h"
 
-@interface TJLViewController () <UITextFieldDelegate>
-@property(weak, nonatomic) IBOutlet UITextField *nameField;
-@property(weak, nonatomic) IBOutlet UITextField *emailField;
-@property(weak, nonatomic) IBOutlet UITextField *passwordField;
-@property(weak, nonatomic) IBOutlet UIImageView *nameIndicator;
-@property(weak, nonatomic) IBOutlet UIImageView *emailIndicator;
-@property(weak, nonatomic) IBOutlet UIImageView *passwordIndicator;
-@property(weak, nonatomic) IBOutlet UIButton *createAccountButton;
-@property(strong, nonatomic) UIActivityIndicatorView *activityIndicatorView;
-@property(weak, nonatomic) IBOutlet UIImageView *imageView;
-
+@interface TJLViewController () <UITableViewDataSource, UITableViewDelegate>
+@property(weak, nonatomic) IBOutlet UITableView *tableView;
+@property(strong, nonatomic) NSArray *dataArray;
 @end
 
 @implementation TJLViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.dataArray = @[@"Red", @"Green", @"Blue"];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
 
-    ///We want to constantly monitor the correctness of the text fields so that we can update the indicators in real time. These are arbitrary values used for correctness, you could use whatever you desired.
-    RACSignal *nameSignal = [self.nameField.rac_textSignal map:^id(NSString *value) {
-        return @(value.length > 4);
-    }];
-    RACSignal *emailSignal = [self.emailField.rac_textSignal map:^id(NSString *value) {
-        return @(value.length > 6 && [value rangeOfString:@"@"].location != NSNotFound);
-    }];
-    RACSignal *passwordSignal = [self.passwordField.rac_textSignal map:^id(NSString *value) {
-        return @(value.length > 6);
-    }];
-    ///We want to indicate to the user exactly when each field goes from incorrect to correct, or vice-versa;
-    id (^block)(NSNumber *) = ^id(NSNumber *value) {
-        return value.boolValue ? GreenStar : RedStar;
-    };
-
-    RAC(self.nameIndicator, image) = [nameSignal map:block];
-    RAC(self.emailIndicator, image) = [emailSignal map:block];
-    RAC(self.passwordIndicator, image) = [passwordSignal map:block];
-
-
-    ///Only enable the create account button when each field is filled out correctly.
-    RACSignal *correctnessSignal = [RACSignal combineLatest:@[nameSignal, emailSignal, passwordSignal]
-                                                     reduce:^(NSNumber *name, NSNumber *email, NSNumber *password) {
-                                                         return @((name.boolValue && email.boolValue && password.boolValue));
-                                                     }];
-    ///A RACCommand is used for buttons in place of adding target actions. In this case, we only want the command to be able to execute if the correctnessSignal returns true.
-    ///Here we return a signal block that executes a network request on a background thread. We send an error if there is an error, or the image and `complete` if it is successful.
-    RACCommand *command = [[RACCommand alloc] initWithEnabled:correctnessSignal signalBlock:^RACSignal *(id input) {
-        return [RACSignal startLazilyWithScheduler:[RACScheduler schedulerWithPriority:RACSchedulerPriorityBackground] block:^(id <RACSubscriber> subscriber) {
-            NSError *error;
-            NSData *data = [NSData dataWithContentsOfURL:pictureURL
-                                                 options:NSDataReadingMappedAlways
-                                                   error:&error];
-            if(error) {
-                [subscriber sendError:error];
-            }
-            else {
-                [subscriber sendNext:[UIImage imageWithData:data]];
-                [subscriber sendCompleted];
-            }
-        }];
-    }];
-
-    ///Here we handle any errors that the command sends.
-    [command.errors subscribeNext:^(NSError *x) {
-        NSLog(@"%@", x);
-    }];
-    ///We set the command to be executed whenever the button is pressed.
-    self.createAccountButton.rac_command = command;
-
-    @weakify(self);
-    ///Hide the keyboard whenever the button is pressed. This would be considered a side effect.
-    [[self.createAccountButton rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+    @weakify(self)
+    RACSignal *selectionSignal = [[self rac_signalForSelector:@selector(tableView:didSelectRowAtIndexPath:)] map:^id(RACTuple *value) {
         @strongify(self)
-        [self.nameField resignFirstResponder];
-        [self.emailField resignFirstResponder];
-        [self.passwordField resignFirstResponder];
+        NSIndexPath *path = value.second;
+        return self.dataArray[(NSUInteger)path.row];
     }];
 
-    ///Here we bind the imageView's image property to the signal sent from the command. We flatten it because `executionSignals` is a signal of signals,
-    /// and flattening is the same as merging, so we get one signal that represents the value of all of the signals. Note the delivery on the main thread.
-    RAC(self.imageView, image) = [[[command executionSignals] flatten] deliverOn:[RACScheduler mainThreadScheduler]];
-    ///The activityIndicator will spin while the command is being executed.
-    self.activityIndicatorView = [[UIActivityIndicatorView alloc] init];
-    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithCustomView:self.activityIndicatorView];
-    UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
-    space.width = 20; //make the activity spinner offset from the edge of the nav bar
-    self.activityIndicatorView.color = [UIColor blackColor];
-    self.navigationItem.rightBarButtonItems = @[space, item];
-    ///Since we cannot set the activityIndicators animating property directly, we have to invoke its methods as side effects.
-    [command.executing subscribeNext:^(NSNumber *x) {
-        @strongify(self)
-        if(x.boolValue) [self.activityIndicatorView startAnimating];
-        else [self.activityIndicatorView stopAnimating];
-    }];
+    [self rac_liftSelector:@selector(performSegueWithIdentifier:sender:) withSignals:selectionSignal, [RACSignal return:nil], nil];
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [textField resignFirstResponder];
-    return YES;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dataArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELL" forIndexPath:indexPath];
+
+    cell.textLabel.text = self.dataArray[(NSUInteger)indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    UIViewController *controller = segue.destinationViewController;
+    controller.title = segue.identifier;
 }
 
 @end
